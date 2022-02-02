5d628ea21d0a9e2f4f5cb8066d7b310450160dfc
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-02-02 16:46:47 +0300

    adding

--- Assets/TowerDiffense/Scripts/TDEnemyMover.cs
+++ Assets/TowerDiffense/Scripts/TDEnemyMover.cs
@@ -13,6 +13,10 @@ public class TDEnemyMover : MonoBehaviour
     [SerializeField]
     private float timer = 1f;
 
+    [SerializeField]
+    [Range(0f, 5f)]
+    private float speed = 1.1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,9 +28,23 @@ public class TDEnemyMover : MonoBehaviour
     {
         foreach(TDTile waypoint in path)
         {
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+            transform.LookAt(endPosition);
             print(": "+ waypoint.name);
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(timer);
+
+            while(travelPercent < 1)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+            
+
+            //old but gold
+           // transform.position = waypoint.transform.position;
+           // yield return new WaitForSeconds(timer);
         }
     }
 }
